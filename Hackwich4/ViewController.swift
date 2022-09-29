888281997a7e1662f9c98b23dc7648ef4eb5137b
Stephan93uh <stephan9@hawaii.edu>
2022-09-28 21:58:19 -1000

    Completed Parts 1-9

--- Hackwich4/ViewController.swift
+++ Hackwich4/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTabLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.firstTabLabel.text = "About"
     }
 
 
---
+++ Hackwich4/secondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  secondViewController.swift
+//  Hackwich4
+//
+//  Created by Stephan on 9/19/22.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet weak var secondTabLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
